f0e20f26e20d76387d598421a629bf0fc8cf5b9e
William Liu <will.liu1705@gmail.com>
2025-10-25 21:46:01 -0400

    Add feature to change dimensions of grid
    - the user can now choose how many dimensions they want (through a button)
    - the squares now have dynamically adapting sizes!

--- script.js
+++ script.js
@@ -1,30 +1,60 @@
 let containerDiv = document.querySelector(".container");
 
-for (let i = 0; i < 4; i++) {
-  let rows = document.createElement("div");
-  rows.classList.add("rows");
-  containerDiv.appendChild(rows);
+const CONTAINER_WIDTH = containerDiv.offsetWidth;
+const CONTAINER_HEIGHT = containerDiv.offsetHeight;
+
+function generateDivs(dimensions) {
+
+  const square_width = Math.floor(CONTAINER_WIDTH / dimensions);
+  const square_height = Math.floor(CONTAINER_HEIGHT / dimensions);
+
+  for (let i = 1; i <= dimensions; i++) {
+    let rows = document.createElement("div");
+    rows.classList.add("rows");
+    containerDiv.appendChild(rows);
+  }
+
+  let rowDivs = document.querySelectorAll(".rows");
+  rowDivs.forEach((rowDiv) => {
+    for (let i = 1; i <= dimensions; i++) {
+      let columns = document.createElement("div");
+      columns.classList.add("columns");
+      columns.style.width = `${square_width}px`
+      columns.style.height = `${square_height}px`
+      rowDiv.appendChild(columns);
+    }
+  })
+
+  let columnDivs = document.querySelectorAll(".columns");
+  columnDivs.forEach((columnDiv) => {
+    let listOfClasses = columnDiv.classList;
+    columnDiv.addEventListener("mouseover", () => {
+      listOfClasses.toggle("hover");
+    })
+
+    columnDiv.addEventListener("mouseout", () => {
+      listOfClasses.toggle("hover");
+    })
+  })
 }
 
-let rowDivs = document.querySelectorAll(".rows");
-rowDivs.forEach((rowDiv) => {
-  for (let i = 0; i < 4; i++) {
-    let columns = document.createElement("div");
-    columns.classList.add("columns");
-    rowDiv.appendChild(columns);
-  }
+function clearDivs() {
+  let rowDivs = document.querySelectorAll(".rows");
+  rowDivs.forEach((rowDiv) => {
+    rowDiv.remove();
+  })
+}
+
+let dimensionsButton = document.querySelector("#dimensions-button");
+dimensionsButton.addEventListener("click", (event) => {
+  let userDimensions;
+  do {
+    userDimensions = prompt("How many squares do you want each side to have? (max: 100)");
+  } while (userDimensions <= 0 || userDimensions > 100);
+  clearDivs();
+  generateDivs(userDimensions);
 })
 
-let columnDivs = document.querySelectorAll(".columns");
-columnDivs.forEach((columnDiv) => {
-  let listOfClasses = columnDiv.classList;
-  columnDiv.addEventListener("mouseover", () => {
-    listOfClasses.toggle("hover")
-  })
-
-  columnDiv.addEventListener("mouseout", () => {
-    listOfClasses.toggle("hover")
-  })
-})
+generateDivs(4);
 
 
